f91aadd8fa60566b8455e81d0b8894254392416b
micia01 <miriamyounes.my@gmail.com>
2019-05-08 19:53:21 +0200

    add images in json

--- code/app.js
+++ code/app.js
@@ -1,21 +1,27 @@
-app = {
-init : function(){
-    
-    console.log("init inside app!");
-        $("title").text("New title");
-        $.get("data.json")
-        .done(app.onSuccess)
-        .fail(app.onError);
-      
-},
-onSuccess:function(jsonData){
-    // processa i dati qui!!!
-    console.log(jsonData);
-},
+var app = {
+	init: function() {
+		console.log("init");
+		$.getJSON("floor_list.json")
+		.done(app.onSuccess)
+		.fail(app.onError);
+	},
+	onSuccess:function(jsonData){
+		let list = jsonData.firstFloorList;
+		   list.forEach(element => {
+			   let html = `<div class="card bg-dark text-white">
+  <img class="card-img" src="${element.plan}" alt="Card image">
+  <div class="card-img-overlay">
+    <h5 class="card-title">${element.roomName}</h5>
+    <p class="card-text"></p>
+  </div>
+</div>`;
+		   $("#previews").append(html);
+		   });
+	},
+	onError: function(e) {
+		console.log(`Ajax error: ${JSON.stringify(e)}`);
+	}
 
-onError:function(e){
-    console.log("error!");
-    console.log(JSON.stringify(e));
 }
-}
+
 $(document).ready(app.init);
